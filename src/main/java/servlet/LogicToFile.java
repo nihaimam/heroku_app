813c239cdefff8b06350278533e567ddcaf80b10
Niha <imamniha@gmail.com>
2020-10-22 14:13:36 -0400

    back to old logic

--- src/main/java/servlet/LogicToFile.java
+++ src/main/java/servlet/LogicToFile.java
@@ -19,74 +19,65 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.WebServlet;
 
 @WebServlet(name = "LogicToFile", urlPatterns = {"/logison"})
-public class LogicToFile extends HttpServlet {
+public class LogicToFile extends HttpServlet
+{
 
-  static enum Data {AGE, NAME};
-  //static String RESOURCE_FILE = "entries.json";
-  static String OperationAdd = "Submit";
+	public void doGet (HttpServletRequest request, HttpServletResponse response) 
+		throws ServletException, IOException
+	{
+		response.setContentType("text/html"); // Tells the web container what we're sending back
+                PrintWriter out = response.getWriter(); // Make it appear as if we're "writing" to the browser window
+		
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Logic Predicate Servlet</title>");
+		out.println("</head>");
+		
+		out.println("<style>");
+		out.println("body { background-color: #D1F2EB; font-family: sans-serif; }");
+		out.println("form { margin: 0 auto; width: 400px; padding: 2em; border: 2px solid #000; border-radius: 5em; background-color: #FFFFFF; }");
+		out.println("ul { list-style: none; padding: 0; margin: 0; }");
+		out.println("li { margin-top: 1em; }");
+		out.println("label { display: inline-block; width: 90px; text-align: right; }");
+		out.println("input{ font: 1em sans-serif; width: 275px; box-sizing: border-box; border: 2px solid #999; background-color: #D1F2EB; }");
+		out.println("h1{ color: black; text-align: center; top: 3%; }");
+		out.println(".button { padding-left: 90px; }");
+		out.println("button { margin-left: 0.5em; }");
+		out.println("</style>");
 
-  @Override
-  public void doGet (HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException
-  {
-    response.setContentType("text/html");
-    PrintWriter out = response.getWriter();
-    printHead(out);
-    printBody(out, "", "", "");
-    printTail(out);
-  }
+		out.println("<body>");
+		out.println("<h1>** SWE 432 - Assignment 5 **</h1>");
+		out.println("<br>");
+		out.println("<ul>");
+		out.println("<form method=\"post\">");
+		out.println("<li>");
+		out.println("<p><strong>TRUTH TABLE GENERATOR</strong><br>please enter a variable and an operator <br><br>allowed operators are <br>(and)  ^ / & / && / and<br>(or)   v / | / or<br>(xor)  xor / ⊕<br>(not)	 ! / ~</p><br>");
+		out.println("</li>");
+		out.println("<li>");
+		out.println("<label for='variable_1'>Variable 1:</label>");
+		out.println("<input type='text' id='v1' name='v1'/>");
+		out.println("</li>");
+		out.println("<li>");
+		out.println("<label for='operation'>Operator:</label>");
+		out.println("<input type='text' id='op' name='op'/>");
+		out.println("</li>");
+		out.println("<li>");
+		out.println("<label for='variable_2'>Variable 2:</label>");
+		out.println("<input type='text' id='v2' name='v2'/>");
+		out.println("</li>");
+		out.println("<li class='button'>");
+		out.println("<button type='submit'>Submit</button>");
+		out.println("</li");
+		out.println("<li>");
+		out.println("<br><br>");
+		out.println("<p><strong>CONTRIBUTION SUMMARY</strong><br>Beth, Tori, Niha<br><br>we all did stuff</p>");
+		out.println("</li>");
+		out.println("</ul>");
+		out.println("</form>");
+		out.println("</body>");
+		out.println("</html>");
 
-  private void printHead (PrintWriter out)
-  {
-    out.println("<html>");
-    out.println("<head>");
-    out.println("<title>Truth Table</title>");
-    out.println("<style>");
-    out.println("body { background-color: #D1F2EB; font-family: sans-serif; }");
-    out.println("table { margin: 0 auto; width: 400px; padding: 2em; border: 2px solid #000; border-radius: 5em; background-color: #FFFFFF; }");
-    out.println("td, th { text-align: left; padding: 10px; box-sizing: border-box; border: 2px solid #999; font: 1em sans-serif; }");
-    out.println("h1, p { color: black; text-align: center; top: 3%; }");
-    out.println("</style>");
-    out.println("</head>");
-  }
-
-  private void printBody (PrintWriter out, String name, String age, String error)
-  {
-    out.println("<body onLoad=\"setFocus()\">");
-    out.println("<h1>** SWE 432 - Assignment 5 **</h1>");
-    out.println("<br>");
-    out.println("<ul>");
-
-    out.print  ("<form name=\"logic2file\" method=\"post\"");
-    //out.println(" action=\""+Domain+Path+Servlet+"\">");
-    out.println("<li>");
-    out.println("<p><strong>TRUTH TABLE GENERATOR</strong><br>please enter a variable and an operator <br><br>allowed operators are <br>(and)  ^ / & / && / and<br>(or)   v / | / or<br>(xor)  xor / ⊕<br>(not)	 ! / ~</p><br>");
-    out.println("</li>");
-
-    out.println(" <table>");  
-    out.println("  <tr>");
-    out.println("   <td>Name:</td>");  
-    out.println("   <td><input type=\"text\" name=\""+Data.NAME.name()  +"\" value=\""+name+"\" size=30 required></td>");
-    out.println("  </tr>");
-    out.println("  <tr>");
-    out.println("   <td>Age:</td>");
-    out.println("   <td><input type=\"text\"  name=\""+Data.AGE.name() +"\" oninput=\"this.value=this.value.replace(/[^0-9]/g,'');\" value=\""  +age+"\" size=3 required></td>");
-    out.println("  </tr>");
-    out.println(" </table>");
-
-    out.println(" <br>");
-    out.println(" <br>");
-    out.println(" <input type=\"submit\" value=\"" + OperationAdd
-      + "\" name=\"Operation\">");
-    out.println(" <input type=\"reset\" value=\"Reset\" name=\"reset\">");
-    out.println("</form>");
-    out.println("</ul>");
-    out.println("</body>");
-  }
-
-private void printTail (PrintWriter out){
-     out.println("");
-     out.println("</html>");
-  }
+		out.close();
+	}  // end doGet()
 
 }
