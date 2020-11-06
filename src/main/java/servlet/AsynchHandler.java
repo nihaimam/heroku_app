83997a00702e9da0b5c2827ac5cd11c3f91c6ccc
Niha <imamniha@gmail.com>
2020-11-06 15:33:35 -0500

    asynchronous logic

---
+++ src/main/java/servlet/AsynchHandler.java
@@ -0,0 +1,41 @@
+//Import Servlet Libraries
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+//Import Java Libraries
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+@WebServlet(name = "Predicate Asynch Serv", urlPatterns = {"/AsynchHandler"})
+public class LogicAsynchServ extends HttpServlet
+{
+	public void processRequest(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException
+   {
+//      response.setContentType("TEXT/HTML"); // Not needed to respond to ajax
+      PrintWriter out = response.getWriter();
+
+      String Nm  = "StringSoFar";
+      out.print("Sorry, I have no suggestions: <FONT COLOR=green>");
+      out.print(Nm);
+      out.print("</FONT>\n");
+
+      out.close();
+   }
+
+    // Method doPost - just calls processRequest
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+       processRequest(request, response);
+    }
+
+    // Method doGet - just calls processRequest
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+       processRequest(request, response);
+    }
+
+}
--- src/main/java/servlet/predicate.js
+++ src/main/java/servlet/predicate.js
@@ -1,52 +1,24 @@
 //global XML request variable
 var xhr
 
-function showV1() //Called from HTML onkeyup for variable 1
+function loadPost()
 {
-	var v1 = document.getElementById("v1").value;
-	document.getElementById("eq_v1").innerHTML = v1;
-}
-
-function showOp() //Called from HTML onkeyup for operator
-{
-	var op = document.getElementById("op").value;
-	document.getElementById("eq_op").innerHTML = op;
-}
-
-function showV2() //Called from HTML onkeyup for variable 2
-{
-	var v2 = document.getElementById("v2").value;
-    document.getElementById("eq_v2").innerHTML = v2;
-} 
-
-
-/*
-	
-	//input taken in
-	xhr=GetXmlHttpObject();
-
-	//error in creation
-	if (xhr == null) {
+	xhr = GetXmlHttpObject();
+	if (xhr == null)
+	{
 		alert("Your browser does not support XMLHTTP!");
 		return;
 	}
+	
+	// Backend handler 
 
-	//response - backend handler
-	var url = "https://cs.gmu.edu:8443/swe432-assign5-ntb.herokuapp.com/AsynchHandler";
-	url = url + "?V1SoFar=" + str;
-	url = url + "&sid=" + Math.random();
-
-	//callback function when server responds
-	xhr.onreadystatechange = stateChanged;
-	xhr.open("GET", url, true); //true == asynchronous
-	xhr.send(null); //send asynchronously
 }
 
-//on return, change the DOM with the response text
-function stateChanged()
+function stateChanged() // On return, change the DOM with the response text
 {
-	if (xhr.readyState == 4) {
-		document.getElementById("table").innerHTML = xhr.responseText;
+	if (xhr.readyState == 4)
+	{
+		document.getElementById("a_v1").innerHTML = xhr.responseText;
 	}
 }
 
@@ -57,7 +29,6 @@ function GetXmlHttpObject()
 	{
 		return new XMLHttpRequest();
 	}
-	
 	//IE6, IE5
 	if (window.ActiveXObject)
 	{
@@ -65,4 +36,3 @@ function GetXmlHttpObject()
 	}
 	return null;
 }
-*/
